238f86dfb14e8cae764a607453642668de402945
Hector Caballero <caballero.22.809@gmail.com>
2022-08-26 11:52:21 -0600

    Add Generic Repository & Unit Of Work services

---
+++ ArmediaTest.DAL/Services/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using ArmediaTest.DAL.Context;
+using ArmediaTest.DAL.Entities;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmediaTest.DAL.Services
+{
+    public class UnitOfWork
+    {
+        private DbTestContext context = new DbTestContext();
+        private IDbContextTransaction _objTran;
+
+        private GenericRepository<TUser> userRepository;
+
+        public GenericRepository<TUser> UserRepository
+        {
+            get
+            {
+                if (userRepository == null)
+                {
+                    userRepository = new GenericRepository<TUser>(context);
+                }
+                return userRepository;
+            }
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
