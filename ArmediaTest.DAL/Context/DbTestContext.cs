869421772c6f600eb7a8d3cd2aefe04c17dae107
Hector Caballero <caballero.22.809@gmail.com>
2022-08-26 10:40:01 -0600

    Add data-aceess-layer (DAL) with DbContext & entites

---
+++ ArmediaTest.DAL/Context/DbTestContext.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ArmediaTest.DAL.Entities;
+
+namespace ArmediaTest.DAL.Context
+{
+    public partial class DbTestContext : DbContext
+    {
+        public DbTestContext()
+        {
+        }
+
+        public DbTestContext(DbContextOptions<DbTestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TRol> TRols { get; set; } = null!;
+        public virtual DbSet<TUser> TUsers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=myserver809.database.windows.net;database=TestCrud;user=admin809;password=azure@2022*;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TRol>(entity =>
+            {
+                entity.HasKey(e => e.CodRol)
+                    .HasName("PK__tRol__F13B12111DE33E6D");
+
+                entity.ToTable("tRol");
+
+                entity.Property(e => e.CodRol).HasColumnName("cod_rol");
+
+                entity.Property(e => e.SnActivo)
+                    .HasColumnName("sn_activo")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.TxtDesc)
+                    .HasMaxLength(500)
+                    .IsUnicode(false)
+                    .HasColumnName("txt_desc");
+            });
+
+            modelBuilder.Entity<TUser>(entity =>
+            {
+                entity.HasKey(e => e.CodUsuario)
+                    .HasName("PK__tUsers__EA3C9B1A82F1AAF6");
+
+                entity.ToTable("tUsers");
+
+                entity.Property(e => e.CodUsuario).HasColumnName("cod_usuario");
+
+                entity.Property(e => e.CodRol).HasColumnName("cod_rol");
+
+                entity.Property(e => e.NroDoc)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("nro_doc");
+
+                entity.Property(e => e.SnActivo)
+                    .HasColumnName("sn_activo")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.TxtApellido)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("txt_apellido");
+
+                entity.Property(e => e.TxtNombre)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("txt_nombre");
+
+                entity.Property(e => e.TxtPassword)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("txt_password");
+
+                entity.Property(e => e.TxtUser)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("txt_user");
+
+                entity.HasOne(d => d.CodRolNavigation)
+                    .WithMany(p => p.TUsers)
+                    .HasForeignKey(d => d.CodRol)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_user_rol");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
